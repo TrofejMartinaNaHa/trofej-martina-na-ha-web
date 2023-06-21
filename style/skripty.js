2b12a5306ce5b010f0df9bf863e972d56eaa638a
Dmitriy Lukin <41737018+konoknir@users.noreply.github.com>
2023-06-22 00:51:42 +0200

    init

---
+++ style/skripty.js
@@ -0,0 +1,83 @@
+function scrollto_byid(obj_id, sp){
+	if(!$(obj_id).length){return false}
+	if(typeof sp==undefined) var sp = 500;
+	$('html, body').animate({
+		scrollTop: $(obj_id).offset().top
+	}, sp);
+}
+
+function countToDate(domIdSelector, toDate, finishMessage){
+	
+	// Set the date we're counting down to
+	var countDownDate = new Date(toDate).getTime();
+	 
+	// Update the count down every 1 second
+	var x = setInterval(function() {
+	 
+		// Get todays date and time
+		var now = new Date().getTime();
+	 
+		// Find the distance between now an the count down date
+		var distance = countDownDate - now;
+	 
+		// Time calculations for days, hours, minutes and seconds
+		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	 
+		// Display the result in the element with id="demo"
+		document.getElementById(domIdSelector).innerHTML = days + " dní " + hours + " hodin "
+		+ minutes + " minut " + seconds + " sekund";
+	 
+		// If the count down is finished, write some text 
+		if (distance < 0) {
+			clearInterval(x);
+			document.getElementById(domIdSelector).innerHTML = finishMessage;
+		}
+	}, 1000);
+	
+	
+}
+
+
+
+$(function(){
+	// Poznamka
+	/*
+		Poznamka
+		na nekolik radku
+	*/
+	
+	$(".scroll_button").click( function(){
+		var odkaz = $(this).attr('href');
+		if(odkaz.length<2 || !$(odkaz).length){
+			return false;
+		}
+		scrollto_byid(odkaz);
+	});
+
+	$('.gallery-item').magnificPopup({
+		type:'image',
+		gallery:{
+			enabled: true
+		}
+	});
+	
+	$('#ytlogo1').magnificPopup({
+    items: {
+      src: 'https://www.youtube.com/watch?v=JKCQGhUms7w'
+    },
+    type: 'iframe' // this is default type
+});
+
+	
+	$('#myTabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+
+countToDate('dday', "September 11, 2022 14:00:00", "HURA!");
+	
+});
+
